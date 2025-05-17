cade87ddd563fa1321525241ebb505e90e9bfe13
Michele <michele.colnaghi@liceobanfi.eu>
2025-05-17 14:07:43 +0200

    movimento nonna, sfondo

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 import pygame
 from pygame.locals import *
-
+from player import Nonna
 
 pygame.init()
+
+sfondo = pygame.image.load('sfondo_spaziale.png')
+gameover = pygame.image.load('game over.png')
+
+
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Nonna Rampage")
 clock = pygame.time.Clock()
-
-color = (245, 235, 220)
+fps = 60
+nonna = Nonna((150, 300))
 running = True
+
+def disegna_oggetti():
+    screen.blit(sfondo, (0,0))
+    screen.blit(nonna.image, (nonna.x, nonna.y))
+
+def aggiorna():
+    pygame.display.update()
+    clock.tick(fps)
+
 
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_w):
+            nonna.y -= nonna.vy
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_s):
+            nonna.y += nonna.vy
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_a):
+            nonna.x -= nonna.vx
+        if (event.type == pygame.KEYDOWN and event.key == pygame.K_d):
+            nonna.x += nonna.vx
+
+    disegna_oggetti()
+
+    aggiorna()
+
+
     
-    screen.fill(color)
-    pygame.display.flip()
-    clock.tick(60)
+    #pygame.display.flip()
+    #clock.tick(60)
 
 pygame.quit()
--- player.py
+++ player.py
@@ -1,15 +1,20 @@
 import pygame
 
 class Nonna:
-    def __init__(self, screen, pos, platform):
-        self.screen = screen
-        self.platform = platform
-        self.image = pygame.image.load()
+    def __init__(self, pos = []):        #a cosa serve platofrm e screen? l'ho tolto poi vediamo
+        #self.screen = screen
+        #self.platform = platform
+        immagine_originale = pygame.image.load("nonna.jpeg")
+        nuova_larghezza = 70
+        nuova_altezza = 70
+        self.image = pygame.transform.scale(immagine_originale, (nuova_larghezza, nuova_altezza))
         self.rect = self.image.get_rect()
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
+        self.x = pos[0]
+        self.y = pos[1]
 
-        self.vx = 0
-        self.vy = 0
-        self.g= 0.5
+        self.vx = 8
+        self.vy = 8
+        #self.g= 0.5 #cos'è?
         
+    
+    
